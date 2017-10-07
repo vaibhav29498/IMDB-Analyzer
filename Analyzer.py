62c0b1d4484f8e4221b0539e4deb215b5642492d
Vaibhav Jain <vaibhav29498@gmail.com>
2017-10-07 20:38:16 +0530

    Initial commit
    Add basic web-scraping code

---
+++ Analyzer.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+# import timeit
+
+print("Enter your user id:", end='')
+userId = input()
+# startTime = timeit.default_timer()
+url = 'http://www.imdb.com/user/ur' + userId + '/ratings'
+baseUrl = url
+page = urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+pageNo = re.search(r'Page [0-9]+ of ([0-9]+)', soup.find('div', 'desc').text)
+if pageNo is None:
+    pageNo = 1
+else:
+    pageNo = int(pageNo.group(1))
+number = imdbRating = yourRating = 0
+curPage = 1
+while curPage <= pageNo:
+    ratingsList = soup.find_all('div', 'list_item')
+    number += len(ratingsList)
+    for item in ratingsList:
+        name = item.find_next('b').find('a').text
+        rating1 = float(re.search(r'Users rated this ([0-9]+.[0-9])', item.find('div', 'rating-list').get('title')).group(1))
+        rating2 = int(re.search(r'rated this ([0-9]+).', item.find('div', 'quoted_rating').text).group(1))
+        imdbRating += rating1
+        yourRating += rating2
+        # print(name, rating2, rating1)
+    if curPage != pageNo:
+        url = baseUrl + '?start=' + str(curPage * 100 + 1) + '&view=detail&sort=ratings_date:desc'
+        page = urlopen(url)
+        soup = BeautifulSoup(page, 'html.parser')
+    curPage += 1
+print('Total reviews:', number)
+print('Your average rating: %.2f' % (yourRating / number))
+print('IMDB average rating: %.2f' % (imdbRating / number))
+# print(timeit.default_timer() - startTime)
